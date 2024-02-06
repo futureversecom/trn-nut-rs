94d9d91f88762823a0f51c92cba0bc4b30ed7e0e
zees-dev <dev.786zshan@gmail.com>
2024-02-07 11:37:59 +1300

    cleanup and updated js lib

--- js/src/lib.rs
+++ js/src/lib.rs
@@ -3,7 +3,7 @@
 //! Provide JS-Rust API bindings to create and inspect TRNNut
 use parity_scale_codec::{Decode, Encode};
 use trnnut_rs::{
-    v0::{contract::Contract, module::Module, TRNNutV0},
+    v0::{method::Method, module::Module, TRNNutV0},
     TRNNut,
 };
 use wasm_bindgen::prelude::*;
@@ -14,81 +14,75 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[inline]
-fn from_slice_32(bytes: &[u8]) -> [u8; 32] {
-    let mut array = [0; 32];
-    if bytes.len() < 32 {
-        log("expected 32 byte array");
-        return array;
-    }
-    let bytes = &bytes[..array.len()]; // panics if not enough data
-    array.copy_from_slice(bytes);
-    array
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-/// A js handle for a rust versioned trnnut struct
+#[wasm_bindgen(typescript_custom_section)]
+const MODULE_METHOD_TYPE_DEFINITION: &str = r#"
+/** Method configuration for modules. */
+export type ModuleMethod = {
+    /** Method name */
+    readonly name: string;
+    /** Block cooldown */
+    readonly blockCooldown?: number;
+    /** Constraints */
+    readonly constraints?: Uint8Array;
+};
+"#;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(typescript_type = "ModuleMethod")]
+    pub type ModuleMethodJS;
+}
+
+#[wasm_bindgen(typescript_custom_section)]
+const MODULE_TYPE_DEFINITION: &str = r#"
+/** Module configuration.  */
+export type Module = {
+    /** Module name */
+    readonly name: string;
+    /** Block cooldown */
+    readonly blockCooldown?: number;
+    /** Methods - i.e. module extrinsics */
+    readonly methods: ReadonlyArray<ModuleMethod>;
+};
+"#;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(typescript_type = "Module")]
+    pub type ModuleJS;
+}
+
+/// A js handle/wrapper for a rust versioned trnnut struct
 #[wasm_bindgen(js_name = TRNNut)]
-pub struct JsHandle(TRNNut);
+pub struct TRNNutJS(TRNNut);
 
 #[wasm_bindgen(js_class = TRNNut)]
 #[allow(irrefutable_let_patterns)]
-impl JsHandle {
+impl TRNNutJS {
     #[wasm_bindgen(constructor)]
     /// Create a new TRNNut, it is always v0 for now
-    pub fn new(modules: &JsValue, contracts: &JsValue) -> Self {
+    pub fn new(modules: &JsValue) -> Self {
         console_error_panic_hook::set_once();
-        let modules_vec: Vec<(String, Module)> = modules
+
+        let modules: Vec<Module> = modules
             .into_serde()
             .expect("Deserialization of modules failed");
-        let contract_vec: Vec<([u8; 32], Contract)> = contracts
-            .into_serde()
-            .expect("Deserialization of contracts failed");
-        let trnnut: TRNNutV0 = TRNNutV0 {
-            modules: modules_vec,
-            contracts: contract_vec,
-        };
-        JsHandle(TRNNut::V0(trnnut))
+
+        let trnnut: TRNNutV0 = TRNNutV0 { modules };
+        TRNNutJS(TRNNut::V0(trnnut))
     }
 
-    #[allow(non_snake_case)]
-    /// Return the trnnut module
-    pub fn getModule(&self, module: &str) -> JsValue {
+    #[wasm_bindgen(js_name = getModule)]
+    pub fn get_module(&self, module: &str) -> JsValue {
         if let TRNNut::V0(trnnut) = &self.0 {
             if trnnut.get_module(module).is_none() {
                 return JsValue::UNDEFINED;
             }
             return JsValue::from_serde(&trnnut.get_module(module).unwrap()).unwrap();
-        }
-        panic!("unsupported trnnut version");
-    }
-
-    #[allow(non_snake_case)]
-    /// Return the trnnut contract
-    pub fn getContract(&self, contract_address: &[u8]) -> JsValue {
-        if let TRNNut::V0(trnnut) = &self.0 {
-            if trnnut
-                .get_contract(from_slice_32(contract_address))
-                .is_none()
-            {
-                return JsValue::UNDEFINED;
-            }
-            return JsValue::from_serde(
-                &trnnut
-                    .get_contract(from_slice_32(contract_address))
-                    .unwrap(),
-            )
-            .unwrap();
-        }
-        panic!("unsupported trnnut version");
-    }
-
-    #[allow(non_snake_case)]
-    /// Verify trnnut is valid for contract_address
-    pub fn verifyContract(&self, contract_address: &[u8]) -> bool {
-        if let TRNNut::V0(trnnut) = &self.0 {
-            return trnnut
-                .validate_contract(from_slice_32(contract_address))
-                .is_ok();
         }
         panic!("unsupported trnnut version");
     }
@@ -99,9 +93,9 @@ impl JsHandle {
     }
 
     /// Decode a version 0 trnnut from `input` bytes
-    pub fn decode(input: &[u8]) -> Result<JsHandle, JsValue> {
+    pub fn decode(input: &[u8]) -> Result<TRNNutJS, JsValue> {
         match TRNNut::decode(&mut &input[..]) {
-            Ok(trnnut) => Ok(JsHandle(trnnut)),
+            Ok(trnnut) => Ok(TRNNutJS(trnnut)),
             Err(err) => {
                 log(&format!("failed decoding: {:?}", err));
                 Err(JsValue::undefined())
@@ -109,3 +103,59 @@ impl JsHandle {
         }
     }
 }
+
+// /// A js handle/wrapper for a rust versioned module
+// #[derive(serde::Deserialize)]
+// #[wasm_bindgen(js_name = Module)]
+// pub struct ModuleJS(Module);
+
+// #[wasm_bindgen(js_class = Module)]
+// #[allow(irrefutable_let_patterns)]
+// impl ModuleJS {
+//     #[wasm_bindgen(constructor)]
+//     /// Create a new Module
+//     pub fn new(name: &str, block_cooldown: u32, methods: JsValue) -> Self {
+//         console_error_panic_hook::set_once();
+
+//         let methods: Vec<Method> = methods
+//             .into_serde()
+//             .expect("Deserialization of methods failed");
+
+//         let module = Module::new(name)
+//             .block_cooldown(block_cooldown)
+//             .methods(vec![]);
+
+//         ModuleJS(module)
+//     }
+
+//     /// Encode the module into bytes
+//     pub fn encode(&mut self) -> Vec<u8> {
+//         self.0.encode()
+//     }
+// }
+
+// /// A js handle/wrapper for a rust versioned Method struct
+// #[derive(serde::Deserialize)]
+// #[wasm_bindgen(js_name = Method)]
+// pub struct MethodJS(Method);
+
+// #[wasm_bindgen(js_class = Method)]
+// #[allow(irrefutable_let_patterns)]
+// impl MethodJS {
+//     #[wasm_bindgen(constructor)]
+//     /// Create a new Method
+//     pub fn new(name: &str, block_cooldown: u32, constraints: Vec<u8>) -> Self {
+//         console_error_panic_hook::set_once();
+        
+//         let method = Method::new(name)
+//             .block_cooldown(block_cooldown)
+//             .constraints(constraints);
+        
+//         MethodJS(method)
+//     }
+
+//     /// Encode the method into bytes
+//     pub fn encode(&mut self) -> Vec<u8> {
+//         self.0.encode()
+//     }
+// }
